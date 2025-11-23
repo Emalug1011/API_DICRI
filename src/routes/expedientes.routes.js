bab74355a0e365519667b17c087315de5bb50d36
emalug <40847621+Emalug1011@users.noreply.github.com>
2025-11-23 13:32:46 -0600

    Se agrega el servicio para reportes

--- src/routes/expedientes.routes.js
+++ src/routes/expedientes.routes.js
@@ -5,7 +5,8 @@ import {
   obtenerExpediente,
   listarExpedientes,
   obtenerExpedientesPorUsuario,
-  obtenerAuditoriaExpediente
+  obtenerAuditoriaExpediente,
+  resumenEstados 
 } from "../controllers/expedientes.controller.js";
 
 import { crearIndicio } from "../controllers/indicios.controller.js";
@@ -13,25 +14,36 @@ import { verificarToken } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-// PROTEGER TODAS LAS RUTAS
+// ------------------------------------------------------
+//  PROTEGER TODAS LAS RUTAS
+// ------------------------------------------------------
 router.use(verificarToken);
 
-// Bandeja Usuario
+// ------------------------------------------------------
+//  BANDEJA DEL USUARIO
+// ------------------------------------------------------
 router.get("/mis-expedientes", obtenerExpedientesPorUsuario);
 
-// Expedientes
+// ------------------------------------------------------
+//  EXPEDIENTES
+// ------------------------------------------------------
+router.get("/resumen", resumenEstados);
 router.post("/", crearExpediente);
-router.get("/", listarExpedientes);
+router.get("/filter", listarExpedientes);
 router.get("/:id", obtenerExpediente);
+
+
+// Cambio de estado
 router.put("/:id/estado", cambiarEstado);
 
-
-// Auditoría
+// ------------------------------------------------------
+//  AUDITORÍA DE EXPEDIENTE
+// ------------------------------------------------------
 router.get("/:id/auditoria", obtenerAuditoriaExpediente);
 
-// Indicios dentro del expediente
+// ------------------------------------------------------
+//  INDICIOS DEL EXPEDIENTE
+// ------------------------------------------------------
 router.post("/:id/indicios", crearIndicio);
-
-
 
 export default router;
